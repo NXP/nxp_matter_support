b747aaa1040357b3dec79ebe2b095ca1834bd441
marius-alex-tache <marius.tache@nxp.com>
2024-07-11 14:11:46 +0300

    [k32w0] Add missing changes related to submodule transition
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>

--- examples/platform/k32w0/scripts/sign-outdir.py
+++ examples/platform/k32w0/scripts/sign-outdir.py
@@ -1,30 +1,58 @@
 import argparse
 import os
+import platform
 import subprocess
 
+def sign_cmd(tool, image, identifer):
+    cmd = f"python {tool} -i={identifer} {image}"
+    if args.simple_hash:
+        cmd = cmd + " -SimpleHashVerification"
+    cmd += f" && arm-none-eabi-objcopy -I srec -O binary {image}.srec {image}.bin"
+
+    return cmd
+
+def windows_sign_cmd(tool_path):
+    # sign_images is a Linux wrapper over dk6_image_tool.py.
+    # On Windows, use the Python script directly.
+    tool = f"{tool_path}\..\dk6_image_tool.py"
+
+    image = [f for f in os.listdir(os.getcwd()) if f.startswith('chip-') and f.endswith('-example')][0]
+    cmd = sign_cmd(tool, image, 1)
+
+    if args.ota_enabled:
+        # Also sign the SSBL with the correct image identifier.
+        cmd += " && " + sign_cmd(tool, 'chip-k32w0x-ssbl', 0)
+
+    return cmd
+
+def linux_sign_cmd(tool_path):
+    # Convert script to unix format if needed
+    subprocess.call("(file " + tool_path + " | grep CRLF > /dev/null) && (dos2unix " + tool_path + ")", shell=True)
+    cmd = tool_path + " " + os.getcwd()
+    if args.simple_hash:
+        cmd = cmd + " -SimpleHashVerification"
+
+    return cmd
 
 def main(args):
     if "NXP_K32W0_SDK_ROOT" in os.environ and os.environ["NXP_K32W0_SDK_ROOT"] != "":
         path_prefix = ""
         # Treat particular case of CI
-        if "third_party/nxp/github_sdk/k32w0/repo" in os.environ["NXP_K32W0_SDK_ROOT"]:
+        if "third_party/nxp/nxp_matter_support/github_sdk/k32w0/repo" in os.environ["NXP_K32W0_SDK_ROOT"]:
             path_prefix = "/core"
-        sign_images_path = os.environ["NXP_K32W0_SDK_ROOT"] + path_prefix + "/tools/imagetool/sign_images.sh"
+        tool_path = os.environ["NXP_K32W0_SDK_ROOT"] + path_prefix + "/tools/imagetool/sign_images.sh"
     else:
-        sign_images_path = os.path.abspath(
-            __file__ + "/../../../../../../third_party/nxp/github_sdk/k32w0/repo/core/tools/imagetool/sign_images.sh")
+        tool_path = os.path.abspath(
+            __file__ + "/../../../../../github_sdk/k32w0/repo/core/tools/imagetool/sign_images.sh")
 
     # Give execute permission if needed
-    if os.access(sign_images_path, os.X_OK) is False:
-        os.chmod(sign_images_path, 0o766)
+    if os.access(tool_path, os.X_OK) is False:
+        os.chmod(tool_path, 0o766)
 
-    # Convert script to unix format if needed
-    subprocess.call("(file " + sign_images_path + " | grep CRLF > /dev/null) && (dos2unix " + sign_images_path + ")", shell=True)
-
-    # Call sign_images.sh script with the output directory
-    cmd = sign_images_path + " " + os.getcwd()
-    if args.simple_hash:
-        cmd = cmd + " -SimpleHashVerification"
+    if platform.system() == "Windows":
+        cmd = windows_sign_cmd(tool_path)
+    else:
+        cmd = linux_sign_cmd(tool_path)
 
     subprocess.call(cmd, shell=True)
 
@@ -34,6 +62,11 @@ if __name__ == "__main__":
     parser.add_argument(
         "--simple-hash",
         help="When enabled, adds a hash of the whole image at the end of the binary.",
+        action="store_true"
+    )
+    parser.add_argument(
+        "--ota-enabled",
+        help="When enabled, OTA is enabled so image identifier should be 1 for app and 0 for SSBL.",
         action="store_true"
     )
     args = parser.parse_args()
