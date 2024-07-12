1d7d91b26bf16c56eb2d059a8e0b82fc1b7a4af5
Gatien Chapon <gatien.chapon@nxp.com>
2024-07-12 13:17:50 +0200

    [script] Updating sdk update script to new path + allow force update for k32w
    + Adding new fields to be able to retreive SDK path locations
    
    Signed-off-by: Gatien Chapon <gatien.chapon@nxp.com>

--- scripts/update_nxp_sdk.py
+++ scripts/update_nxp_sdk.py
@@ -21,6 +21,7 @@ import os
 import shutil
 import subprocess
 import sys
+import yaml
 from dataclasses import dataclass
 
 NXP_MATTER_SUPPORT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -29,30 +30,49 @@ NXP_MATTER_SUPPORT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)
 @dataclass(init=False)
 class NxpSdk:
     sdk_name: str
-    sdk_target_location: str
+    sdk_target_location_abspath: str
+    sdk_target_location_relativepath_from_root: str
     sdk_manifest_path: str
+    sdk_manifest_name: str
+    sdk_storage_location_abspath: str
+    sdk_storage_location_relativepath_from_root: str
 
-    def __init__(self, name, sdk_target_location):
+    def __init__(self, name, sdk_target_relative_path):
         self.sdk_name = name
-        self.sdk_target_location = sdk_target_location
-        self.sdk_manifest_path = os.path.abspath(os.path.join(sdk_target_location, 'manifest'))
+        self.sdk_target_location_abspath = os.path.join(NXP_MATTER_SUPPORT_ROOT, sdk_target_relative_path)
+        self.sdk_target_location_relativepath_from_root = sdk_target_relative_path
+        self.sdk_manifest_path = os.path.abspath(os.path.join(self.sdk_target_location_abspath , 'manifest'))
+        self.sdk_manifest_name = 'west.yml'
+        self.get_sdk_storage_location()
 
+    def get_sdk_storage_location(self):
+        westFilePath = os.path.abspath(os.path.join(self.sdk_manifest_path, self.sdk_manifest_name))
+        if (os.path.exists(westFilePath)):
+            westStream = open(westFilePath, 'r')
+            west = yaml.load(westStream, Loader=yaml.SafeLoader)
+            westStream.close()
+            try:
+                path_prefix = west["manifest"]["projects"][0]["import"]["path-prefix"]
+                self.sdk_storage_location_relativepath_from_root = os.path.join(NXP_MATTER_SUPPORT_ROOT, self.sdk_target_location_relativepath_from_root , path_prefix)
+                self.sdk_storage_location_abspath = os.path.abspath(self.sdk_storage_location_relativepath_from_root)
+            except (KeyError) as exception:
+                logging.error("Wrong west file format %s", exception)
+        else:
+            logging.error("SDK west config file : %s does not exist", westFilePath)
+            sys.exit(1)
 
 def NxpSdk_k32w0():
-    rel_path_k32w0 = 'github_sdk/k32w0'
-    sdk = NxpSdk('k32w0', os.path.abspath(os.path.join(NXP_MATTER_SUPPORT_ROOT, rel_path_k32w0)))
+    sdk = NxpSdk('k32w0', 'github_sdk/k32w0')
     return sdk
 
 def NxpSdk_common():
-    rel_path_common = 'github_sdk/rw_k32w1'
-    sdk = NxpSdk('common', os.path.abspath(os.path.join(NXP_MATTER_SUPPORT_ROOT, rel_path_common)))
+    sdk = NxpSdk('common', 'github_sdk/rw_k32w1')
     return sdk
 
 def NxpSdk_rt():
     rel_path_rt = 'github_sdk/rt'
-    sdk = NxpSdk('rt', os.path.abspath(os.path.join(NXP_MATTER_SUPPORT_ROOT, rel_path_rt)))
+    sdk = NxpSdk('rt', 'github_sdk/rt')
     return sdk
-
 
 ALL_PLATFORM_SDK = [
     NxpSdk_k32w0(),
@@ -72,37 +92,34 @@ def clean_sdk_local_changes(sdk_location):
 
 
 def init_nxp_sdk_version(nxp_sdk, force):
-    print("Init SDK in: " + nxp_sdk.sdk_target_location)
-    west_path = os.path.join(nxp_sdk.sdk_target_location, '.west')
+    print("Init SDK in: " + nxp_sdk.sdk_target_location_abspath)
+    west_path = os.path.join(nxp_sdk.sdk_target_location_abspath, '.west')
     if os.path.exists(west_path):
         if not force:
             logging.error("SDK is already initialized, use --force to force init")
             sys.exit(1)
         shutil.rmtree(west_path)
 
-    command = ['west', 'init', '-l', nxp_sdk.sdk_manifest_path, '--mf', 'west.yml']
+    command = ['west', 'init', '-l', nxp_sdk.sdk_manifest_path, '--mf', nxp_sdk.sdk_manifest_name]
     subprocess.run(command, check=True)
     update_nxp_sdk_version(nxp_sdk, force)
 
 
 def update_nxp_sdk_version(nxp_sdk, force):
-    print("Update SDK in " + nxp_sdk.sdk_target_location)
-    if not os.path.exists(os.path.join(nxp_sdk.sdk_target_location, '.west')):
+    print("Update SDK in " + nxp_sdk.sdk_target_location_abspath)
+    if not os.path.exists(os.path.join(nxp_sdk.sdk_target_location_abspath, '.west')):
         logging.error("--update-only error SDK is not initialized")
         sys.exit(1)
     command = ['west', 'update', '--fetch', 'smart']
     try:
-        subprocess.run(command, cwd=nxp_sdk.sdk_target_location, check=True)
+        subprocess.run(command, cwd=nxp_sdk.sdk_target_location_abspath, check=True)
     except (RuntimeError, subprocess.CalledProcessError) as exception:
         if force:
-            if nxp_sdk.sdk_name == 'k32w0':
-                logging.error('Force update not yet supported for %s platform', nxp_sdk.sdk_name)
-            else:
-                # In case of force update option and in case of update failure:
-                # 1. try to clean all local modications if any
-                # 2. Retry the west update command. It should be successfull now as all local modifications have been cleaned
-                clean_sdk_local_changes(nxp_sdk.sdk_target_location)
-                subprocess.run(command, cwd=nxp_sdk.sdk_target_location, check=True)
+            # In case of force update option and in case of update failure:
+            # 1. try to clean all local modications if any
+            # 2. Retry the west update command. It should be successfull now as all local modifications have been cleaned
+            clean_sdk_local_changes(nxp_sdk.sdk_target_location_abspath)
+            subprocess.run(command, cwd=nxp_sdk.sdk_target_location_abspath, check=True)
         else:
             logging.exception(
                 'Error SDK cannot be updated, local changes should be cleaned manually or use --force to force update %s', exception)
