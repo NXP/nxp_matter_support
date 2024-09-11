8f0b090dbc898bc9a16078066adf555ce1904796
marius-alex-tache <marius.tache@nxp.com>
2024-09-11 13:07:11 +0300

    [common] Add common BLE files that are SDK coupled
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>

---
+++ examples/platform/common/ble/ble_function_mux.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2024 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+
+/**
+ *    @file
+ *          Provides an implementation for BLE Host NVM functions
+ */
+
+#ifndef BLE_FUNCTION_MUX_H
+#define BLE_FUNCTION_MUX_H
+
+typedef enum
+{
+    kBleFuncMux_AppMode_None,
+    kBleFuncMux_AppMode_Ota
+} ble_func_mux_app_mode_t;
+
+#endif
