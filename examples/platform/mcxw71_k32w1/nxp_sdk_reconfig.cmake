b02bd133fc113a21a614a71d26bf0a19c9147efc
marius-alex-tache <marius.tache@nxp.com>
2024-12-16 14:19:23 +0200

    [mcxw] Update copyright and license
    Signed-off-by: marius-alex-tache <marius.tache@nxp.com>

--- examples/platform/mcxw71_k32w1/nxp_sdk_reconfig.cmake
+++ examples/platform/mcxw71_k32w1/nxp_sdk_reconfig.cmake
@@ -1,18 +1,5 @@
-#
-#   Copyright (c) 2024 Project CHIP Authors
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-#
+# Copyright 2024 NXP
+# SPDX-License-Identifier: BSD-3-Clause
 
 get_filename_component(MATTER_ROOT ${CMAKE_CURRENT_SOURCE_DIR}/../../../ REALPATH)
 get_filename_component(NXP_MATTER_SUPPORT_ROOT ${MATTER_ROOT}/third_party/nxp/nxp_matter_support REALPATH)
