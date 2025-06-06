66e86e89e71a88dc804397bd7b3f1019086a6f62
Gatien Chapon <gatien.chapon@nxp.com>
2025-06-06 18:19:59 +0200

    [windows-build] Updating the bootstrap script to remove the usage of pigweed and to support cmake build
    Signed-off-by: Gatien Chapon <gatien.chapon@nxp.com>

--- scripts/setup/bootstrap.bat
+++ scripts/setup/bootstrap.bat
@@ -1,214 +1,38 @@
 :<<"::WINDOWS_ONLY"
 @echo off
-:: Copyright 2024 NXP
+:: Copyright 2024,2025 NXP
 :: All rights reserved.
 ::
 :: SPDX-License-Identifier: BSD-3-Clause
 ::
 :<<"::WINDOWS_ONLY"
 
-:: The following Python checks are extracted from the official bootstrap
-:: script found in the Pigweed repository.
-
-:: Allow forcing a specific Python version through the environment variable
-:: PW_BOOTSTRAP_PYTHON. Otherwise, use the system Python if one exists.
-if not "%PW_BOOTSTRAP_PYTHON%" == "" (
-    set "python=%PW_BOOTSTRAP_PYTHON%"
-    goto check_curl
+REM Usage: ".\third_party\nxp\nxp_matter_support\scripts\bootstrap.bat [sdk_path]"
+REM Usage: sdk_path is optional if not given the default SDK path would be assumed
+IF "%~1"=="" (
+    goto default_sdk_path
+) ELSE IF "%~1"=="--platform" (
+    REM Case to ignore case where script is called with .\third_party\nxp\nxp_matter_support\scripts\bootstrap.bat --platform nxp
+    REM Goal is to keep backward compatibility with old boostrap scrip version
+    REM In such case set SDK_PATH to the default value
+    goto default_sdk_path
+) ELSE (
+    SET "SDK_PATH=%~1"
+    goto run_sub_bootstrap
 )
 
-setlocal EnableDelayedExpansion
-:: Detect python installation.
-:: In order to avoid opening Microsoft Store app, bypass the entries that
-:: contain WindowsApps string in their path. This ensures that a proper
-:: Python3 executable is used. If no Python is detected, log a message.
-for /f %%p in ('where python') do (
-    echo.%%p | findstr WindowsApps >NUL 2>&1
-    if !ERRORLEVEL! NEQ 0 (
-        %%p --version >python_version.tmp 2>&1
-        findstr /C:"Python 3" python_version.tmp >NUL 2>&1
-        if !ERRORLEVEL! EQU 0 (
-            endlocal
-            echo Python is found at: %%p
-            set "python=%%p"
-            goto check_curl
-        )
-    )
+:default_sdk_path
+SET "SDK_PATH=third_party\nxp\nxp_matter_support\github_sdk\sdk_next\repo\mcuxsdk"
+
+:run_sub_bootstrap
+echo SDK PATH = "%SDK_PATH%
+
+call third_party\nxp\nxp_matter_support\scripts\setup\sub_bootstrap.bat %~n0
+
+REM Export again environment variables to keep them in the parent process
+IF NOT EXIST temp_env.bat (
+    exit /b 1
+) ELSE (
+    call temp_env.bat
+    del temp_env.bat
 )
-
-echo.
-echo Error: no system Python3 present
-echo.
-echo   Pigweed's bootstrap process requires a local system Python3.
-echo   Please install Python3 on your system, add it to your PATH
-echo   and re-try running bootstrap.
-goto finish
-
-:: Detect curl installation
-:check_curl
-where curl >NUL 2>&1
-if %ERRORLEVEL% NEQ 0 (
-    echo.
-    echo Error: no system curl present
-    echo.
-    echo   Pigweed's prerequisites check requires a local system curl.
-    echo   Please install curl on your system, add it to your PATH
-    echo   and re-try running bootstrap.
-    goto finish
-)
-
-set _BOOTSTRAP_NAME=%~n0
-set _CHIP_ROOT=%cd%
-set _CONFIG_FILE=%_CHIP_ROOT%\third_party\nxp\nxp_matter_support\scripts\setup\environment.json
-
-set PW_BRANDING_BANNER=%_CHIP_ROOT%\scripts\setup\banner.txt
-set PW_DOCTOR_SKIP_CIPD_CHECKS=1
-set PW_PROJECT_ROOT=%_CHIP_ROOT%
-set PW_ENVIRONMENT_ROOT=%PW_PROJECT_ROOT%\.environment
-set PW_ROOT=%PW_PROJECT_ROOT%\third_party\pigweed\repo
-:: Skip pw environment check for now. To be removed in the future.
-set PW_ACTIVATE_SKIP_CHECKS=1
-
-set _PIGWEED_CIPD_JSON_ROOT=%PW_PROJECT_ROOT%\third_party\pigweed\repo\pw_env_setup\py\pw_env_setup\cipd_setup
-set _PW_ACTUAL_ENVIRONMENT_ROOT=%PW_ENVIRONMENT_ROOT%
-set _SETUP_BAT=%_PW_ACTUAL_ENVIRONMENT_ROOT%\activate.bat
-set NXP_SYSTEM_ROOT=C:\NXP
-
-:: Ensure a similar usage to bootstrap.sh for the --platform
-:: option. The only valid option is the 'nxp' platform, since
-:: this is a custom script.
-set _INSTALL_NXP_REQUIREMENTS=0
-if "%1" == "--platform" (
-    if not "%2" == "nxp" (
-        goto usage
-    )
-    set _INSTALL_NXP_REQUIREMENTS=1
-)
-
-if "%_BOOTSTRAP_NAME%" == "bootstrap" (
-    git submodule sync --recursive
-    git submodule update
-)
-
-if not exist "%_PW_ACTUAL_ENVIRONMENT_ROOT%" mkdir "%_PW_ACTUAL_ENVIRONMENT_ROOT%"
-set _GENERATED_PIGWEED_CIPD_JSON=%_PW_ACTUAL_ENVIRONMENT_ROOT%\pigweed.json
-
-:: To maintain backwards compatibility with older Matter versions,
-:: check if the extra arg file exists.
-set _PYTHON_CIPD_JSON="%_PIGWEED_CIPD_JSON_ROOT%\python311.json"
-if exist "%_PYTHON_CIPD_JSON%" (
-    set EXTRA_ARGS=-e windows:%_PYTHON_CIPD_JSON%
-) else (
-    set EXTRA_ARGS=
-)
-call "%python%" "scripts/setup/gen_pigweed_cipd_json.py" -i "%_PIGWEED_CIPD_JSON_ROOT%\pigweed.json" -o "%_GENERATED_PIGWEED_CIPD_JSON%" %EXTRA_ARGS%
-if %ERRORLEVEL% NEQ 0 goto finish
-
-if "%_BOOTSTRAP_NAME%" == "bootstrap" (
-    goto check_developer_mode
-) else (
-    call "%python%" "%PW_ROOT%\pw_env_setup\py\pw_env_setup\windows_env_start.py"
-    if %ERRORLEVEL% NEQ 0 goto finish
-    goto pw_activate
-)
-
-:usage
-echo Usage: "scripts\bootstrap.bat [--platform nxp]"
-goto pw_cleanup
-
-:: Ensure developer mode is enabled
-:check_developer_mode
-echo Checking if Developer Mode is enabled...
-reg query HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock /v AllowDevelopmentWithoutDevLicense | find "0x1" >NUL 2>&1
-if %ERRORLEVEL% EQU 0 goto pw_check_prerequisites
-set /p ENABLE_DEVELOPER_MODE=This step will enable Windows Developer Mode. Are you sure? (y/n)
-if /i "%ENABLE_DEVELOPER_MODE%" NEQ "y" (
-    echo Developer Mode is a prerequisite for pigweed virtual environment. Cancelling bootstrap.
-    goto finish
-)
-goto pw_check_prerequisites
-
-:: Ensure pigweed env prerequisites are OK
-:pw_check_prerequisites
-curl https://pigweed.googlesource.com/pigweed/examples/+/main/tools/setup_windows_prerequisites.bat?format=TEXT > setup_pigweed_prerequisites.b64
-certutil -decode -f setup_pigweed_prerequisites.b64 setup_pigweed_prerequisites.bat
-del setup_pigweed_prerequisites.b64
-call setup_pigweed_prerequisites.bat
-if %ERRORLEVEL% NEQ 0 goto finish
-goto pw_bootstrap
-
-:install_additional_pip_requirements
-echo Installing additional Python modules required by Matter
-pip install -q -r "%_CHIP_ROOT%\scripts\setup\requirements.all.txt" -c "%_CHIP_ROOT%\scripts\setup\constraints.txt"
-if %_INSTALL_NXP_REQUIREMENTS% NEQ 0 (
-    echo Installing additional Python modules required by NXP
-    pip install -q -r "%_CHIP_ROOT%\scripts\setup\requirements.nxp.txt" -c "%_CHIP_ROOT%\scripts\setup\constraints.txt"
-)
-goto setup_sdk_paths
-
-:: Some binaries still have issues with long paths, so temporarily
-:: work around the issue by creating symbolic links for the SDK paths.
-:: This is taken into account by NXP Matter build system.
-:setup_sdk_paths
-if not exist "%NXP_SYSTEM_ROOT%\" (
-    mkdir "%NXP_SYSTEM_ROOT%"
-)
-goto create_sdk_symlink
-
-:: Create a separate step for this action. During bootstrap, it will be set to
-:: the current Matter repository SDK path. On the other hand, if activate is
-:: run inside another Matter repository, the old symlink will be removed and a
-:: new one will be created, which points to the current Matter SDK path.
-:create_sdk_symlink
-if exist "%NXP_SYSTEM_ROOT%\sdk\" (
-    rmdir "%NXP_SYSTEM_ROOT%\sdk\"
-)
-mklink /D "%NXP_SYSTEM_ROOT%\sdk" "%_CHIP_ROOT%\third_party\nxp\nxp_matter_support\github_sdk"
-goto pw_cleanup
-
-:pw_bootstrap
-call "%python%" "%PW_ROOT%\pw_env_setup\py\pw_env_setup\env_setup.py" ^
-    --pw-root "%PW_ROOT%" ^
-    --shell-file "%_SETUP_BAT%" ^
-    --install-dir "%_PW_ACTUAL_ENVIRONMENT_ROOT%" ^
-    --config-file "%_CONFIG_FILE%" ^
-    --project-root "%PW_PROJECT_ROOT%" ^
-    --additional-cipd-file "%_GENERATED_PIGWEED_CIPD_JSON%"
-if %ERRORLEVEL% NEQ 0 goto finish
-goto pw_activate
-
-:pw_activate
-call "%_SETUP_BAT%"
-if "%_BOOTSTRAP_NAME%" == "bootstrap" (
-    goto install_additional_pip_requirements
-) else (
-    goto create_sdk_symlink
-)
-
-:pw_cleanup
-set _PW_BANNER=
-set _PW_BANNER_FUNC=
-set PW_BANNER_FUNC=
-set _PW_ENV_SETUP=
-set _PW_NAME=
-set PW_ENVIRONMENT_ROOT=
-set _PW_PYTHON=
-set _PW_ENV_ROOT_TXT=
-set _PW_PREV_ENV_ROOT=
-set _PW_SETUP_SH=
-set _PW_DEACTIVATE_SH=
-set _NEW_PW_ROOT=
-set _PW_ENV_SETUP_STATUS=
-set _PW_ENV_PREFIX=
-set _PW_ENV=
-set _PW_DOTENV=
-goto finish
-
-:finish
-set _CHIP_ROOT=
-set _BOOTSTRAP_NAME=
-set PW_CIPD_INSTALL_DIR=
-set _PW_TEXT=
-set PW_DOCTOR_SKIP_CIPD_CHECKS=
-if exist setup_pigweed_prerequisites.bat del setup_pigweed_prerequisites.bat
-if exist python_version.tmp del python_version.tmp
---
+++ scripts/setup/sub_bootstrap.bat
@@ -0,0 +1,198 @@
+:<<"::WINDOWS_ONLY"
+@echo off
+:: Copyright 2024,2025 NXP
+:: All rights reserved.
+::
+:: SPDX-License-Identifier: BSD-3-Clause
+::
+:<<"::WINDOWS_ONLY"
+
+setlocal EnableDelayedExpansion
+
+if "%~1" == "activate" (
+    goto python_env
+)
+
+:: Ensure developer mode is enabled
+:check_developer_mode
+echo Checking if Developer Mode is enabled...
+reg query HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock /v AllowDevelopmentWithoutDevLicense | find "0x1" >NUL 2>&1
+if %ERRORLEVEL% EQU 0 goto python_env
+set /p ENABLE_DEVELOPER_MODE=This step will enable Windows Developer Mode. Are you sure? (y/n)
+if /i "%ENABLE_DEVELOPER_MODE%" NEQ "y" (
+    echo Developer Mode is a prerequisite for pigweed virtual environment. Cancelling bootstrap.
+    exit /b 1
+)
+
+:python_env
+echo Python installation ...
+:: Detect python installation.
+:: In order to avoid opening Microsoft Store app, bypass the entries that
+:: contain WindowsApps string in their path. This ensures that a proper
+:: Python3 executable is used. If no Python is detected, log a message.
+for /f %%p in ('where python') do (
+    echo.%%p | findstr WindowsApps >NUL 2>&1
+    if !ERRORLEVEL! NEQ 0 (
+        %%p --version >python_version.tmp 2>&1
+        findstr /C:"Python 3" python_version.tmp >NUL 2>&1
+        if !ERRORLEVEL! EQU 0 (
+            endlocal
+            echo Python is found at: %%p
+            set "python=%%p"
+            goto env_setup
+        )
+    )
+)
+
+echo.
+echo Error: no system Python3 present
+echo.
+echo   Please install Python3 on your system, add it to your PATH
+echo   and re-try running bootstrap.
+exit /b 1
+
+goto env_setup
+
+:: Env setup
+:env_setup
+
+REM Check if the script is being run from the <matter_repo>
+IF NOT EXIST "scripts\setup\zap.version" (
+    echo ERROR: This script must be run from the <matter_repo> directory.
+    exit /b 1
+)
+
+REM Check if pigweed_environment.gni exists
+IF NOT EXIST "build_overrides\pigweed_environment.gni" (
+    echo ERROR: pigweed_environment.gni not found in build_overrides.
+    exit /b 1
+)
+
+REM Check if gn is available in the PATH
+where gn >nul 2>nul
+IF ERRORLEVEL 1 (
+    echo ERROR: gn is not available in the PATH.
+    exit /b 1
+)
+
+REM Check if cmake is available in the PATH
+where cmake >nul 2>nul
+IF ERRORLEVEL 1 (
+    echo ERROR: cmake is not available in the PATH.
+    exit /b 1
+)
+
+set "VENV_PATH=%CD%\venv"
+
+if "%~1" == "activate" (
+    goto activate
+) else (
+    goto boostrap
+)
+
+:: Activate
+:activate
+REM Check if already in a virtual env
+IF DEFINED VIRTUAL_ENV (
+    echo ERROR: already in a virtual env
+    exit /b 1 
+)
+REM Activate the virtual environnemnt
+IF EXIST "venv" (
+    call "venv\Scripts\activate"
+) else (
+    echo ERROR: venv not found would need to run bootstrap script
+    exit /b 1 
+)
+REM Check now that the virtual env is active
+IF DEFINED VIRTUAL_ENV (
+    echo Virtual environment is active.
+    echo VIRTUAL_ENV = %VIRTUAL_ENV%
+) else (
+    echo Virtual environment is NOT active.
+)
+goto end
+
+:: boostrap
+:boostrap
+
+REM Check if requirements.txt exists in the SDK path
+IF NOT EXIST "%SDK_PATH%\scripts\requirements.txt" (
+    echo ERROR: requirements.txt not found in %SDK_PATH%\scripts.
+    exit /b 1
+)
+
+REM Run mcux-env.cmd
+IF NOT EXIST "%SDK_PATH%\mcux-env.cmd" (
+    echo ERROR: mcux-env.cmd not found in %SDK_PATH%.
+    exit /b 1
+)
+
+REM Check if requirements-matter.txt exists in the SDK path
+IF EXIST "%SDK_PATH%\scripts\requirements-matter.txt" (
+    SET "REQUIREMENTS_FILE=%SDK_PATH%\scripts\requirements-matter.txt"
+) ELSE (
+    SET "REQUIREMENTS_FILE="
+)
+
+REM Create Python virtual environment if it doesn't exist
+IF NOT EXIST "venv" (
+    echo Creating Python virtual environment...
+    call "%python%" -m venv venv
+)
+
+REM Activate the virtual environment if not already active
+IF NOT DEFINED VIRTUAL_ENV (
+    call "venv\Scripts\activate"
+)
+
+REM Check now that the virtual env is active
+IF NOT DEFINED VIRTUAL_ENV (
+    echo ERROR: venv was not correclty activated.
+    exit /b 1
+)
+
+REM Install requirements from requirements.txt
+pip install -r "%SDK_PATH%\scripts\requirements.txt"
+
+REM Install additional packages if requirements-matter.txt is missing
+IF "%REQUIREMENTS_FILE%"=="" (
+    echo Installing additional packages...
+    pip install crc>=7.0.0 jsonschema>=4.17.0
+) ELSE (
+    echo Installing requirements from requirements-matter.txt...
+    pip install -r "%REQUIREMENTS_FILE%"
+)
+
+:end
+
+REM Read zap version from file
+FOR /F "usebackq delims=" %%F IN ("scripts\setup\zap.version") DO SET zap_version=%%F
+
+REM Build the full path
+SET "ZAP_INSTALL_PATH=%CD%\.zap\zap-%zap_version%"
+
+REM Use pushd to resolve to absolute path
+IF EXIST "%ZAP_INSTALL_PATH%\" (
+	echo ZAP already exist...
+    pushd "%ZAP_INSTALL_PATH%"
+    SET "ZAP_INSTALL_PATH=%CD%\.zap\zap-%zap_version%"
+    popd
+) ELSE (
+    REM Download ZAP tool if not found
+    echo ZAP not found, downloading...
+    call "%python%" scripts\tools\zap\zap_download.py --sdk-root . --extract-root .zap
+    pushd "%CD%\.zap\zap-%zap_version%"
+    SET "ZAP_INSTALL_PATH=%CD%\.zap\zap-%zap_version%"
+    popd
+)
+
+echo ZAP_INSTALL_PATH is %ZAP_INSTALL_PATH%
+
+call "%SDK_PATH%\mcux-env.cmd"
+
+REM Add ZAP_INSTALL_PATH and VENV_PATH\Scripts to PATH for this session
+echo set "PATH=%ZAP_INSTALL_PATH%;%VENV_PATH%\Scripts;%%PATH%%" > temp_env.bat
+echo set "ZEPHYR_BASE=%ZEPHYR_BASE%" >> temp_env.bat
+
+west mcuxsdk-export
