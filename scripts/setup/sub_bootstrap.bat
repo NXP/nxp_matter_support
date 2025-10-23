f86d6470183b44bdd359f42d61a3b51249bebe34
Dina Benamar <dina.benamarelmaaroufi@nxp.com>
2025-10-23 12:12:19 +0200

    [scripts][common] Updating the bootstrap.bat script to make sure the python_version.tmp is created in a temp location
    Signed-off-by: Dina Benamar <dina.benamarelmaaroufi@nxp.com>

--- scripts/setup/sub_bootstrap.bat
+++ scripts/setup/sub_bootstrap.bat
@@ -33,8 +33,8 @@ echo Python installation ...
 for /f %%p in ('where python') do (
     echo.%%p | findstr WindowsApps >NUL 2>&1
     if !ERRORLEVEL! NEQ 0 (
-        "%%p" --version >python_version.tmp 2>&1
-        findstr /C:"Python 3" python_version.tmp >NUL 2>&1
+        %%p --version >"%TEMP%\python_version.tmp" 2>&1
+        findstr /C:"Python 3" "%TEMP%\python_version.tmp" >NUL 2>&1
         if !ERRORLEVEL! EQU 0 (
             endlocal
             echo Python is found at: %%p
