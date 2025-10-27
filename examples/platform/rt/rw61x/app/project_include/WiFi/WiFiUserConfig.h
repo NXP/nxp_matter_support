29e1d31d8e75ed08838420f00999bb0e097526a0
Martin Girardot <martin.girardot@nxp.com>
2025-10-27 11:07:27 +0100

    [rw61x] add wifi user config file example
    Signed-off-by: Martin Girardot <martin.girardot@nxp.com>

---
+++ examples/platform/rt/rw61x/app/project_include/WiFi/WiFiUserConfig.h
@@ -0,0 +1,17 @@
+/*
+ *  Copyright 2025 NXP
+ *  All rights reserved.
+ *
+ *  SPDX-License-Identifier: BSD-3-Clause
+ */
+
+ /*
+ WiFi user config file example, could be moved at application level
+ */
+#pragma once
+
+/* 
+CONFIG_WIFI_RECOVERY could be define as 1 to enable wifi recovery mechanism, disable by default
+*/
+
+#define CONFIG_WIFI_RECOVERY 0
