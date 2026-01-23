4e9db0dbbb0d0419e1380240440288a24ec4e28f
Marian Chereji <marian.chereji@nxp.com>
2026-01-23 14:09:29 +0200

    [mcxw7x] Use a custom "board_lp.c" implementation
    Preparing the context for supporting PowerDown in Matter as a new low
    power state, the Matter applications on MCXW7x platforms are required
    to use a local, custom implementation of the "board_lp.c" file instead
    of the default implementation in the NXP MCU SDK.
    
    Signed-off-by: Marian Chereji <marian.chereji@nxp.com>

--- examples/platform/project_segments/mcxw7x/prjseg.cmake
+++ examples/platform/project_segments/mcxw7x/prjseg.cmake
@@ -18,8 +18,20 @@ if(CONFIG_MCUX_PRJSEG_module.board.matter.board)
         components/board_comp.c
         dcdc/board_dcdc.c
         ext_flash/board_extflash.c
-        low_power/board_lp.c
     )
+
+    if("${CHIP_ROOT}" STREQUAL "${SdkRootDirPath}/middleware/matter")
+        mcux_add_source(
+            BASE_PATH ${SdkRootDirPath}/middleware/matter/third_party/nxp/nxp_matter_support
+            SOURCES examples/platform/project_segments/mcxw7x/low_power/board_lp.c
+        )
+    else()
+        # Matter not integrated in MCU SDK
+        mcux_add_source(
+            BASE_PATH ${NXP_MATTER_SUPPORT_DIR}
+            SOURCES examples/platform/project_segments/mcxw7x/low_power/board_lp.c
+        )
+    endif()
 
     mcux_add_source(
         BASE_PATH ${SdkRootDirPath}/examples/_boards/${board}/wireless_examples
