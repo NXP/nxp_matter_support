f8e37a2eef3496c02be2f6898ee2aa0f1203a4ac
Divyanshu Kapil <divyanshu.kapil@nxp.com>
2026-02-25 14:26:38 +0530

    [MATTER-3948][EVKC-RT1060][SE05X] Moved GPIO init call before guards

--- examples/platform/rt/rt1060/board/hardware_init.c
+++ examples/platform/rt/rt1060/board/hardware_init.c
@@ -64,11 +64,12 @@ void BOARD_InitHardware(void)
         BOARD_InitM2I2CPins();
 #endif
 
-#if CONFIG_CHIP_SE05X && !defined(WIFI_IW612_BOARD_MURATA_2EL_M2) && !defined(WIFI_IW610_BOARD_MURATA_2LL_M2)
-        BOARD_InitI2CPins();
 #if defined(CONFIG_SE05X_HOST_GPIO)
         BOARD_InitSe05xGpioPins();
 #endif
+
+#if CONFIG_CHIP_SE05X && !defined(WIFI_IW612_BOARD_MURATA_2EL_M2) && !defined(WIFI_IW610_BOARD_MURATA_2LL_M2)
+        BOARD_InitI2CPins();
 #endif
 
 #ifdef BOARD_OTW_K32W0_PIN_INIT
